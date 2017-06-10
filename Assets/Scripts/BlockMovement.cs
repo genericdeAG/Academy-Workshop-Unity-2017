ca18526e4a25250e4ad6e6c7352003d9cd06fb1c
Sebastian Schwarz <sebastian.schwarz@generic.de>
2017-06-10 23:48:19 +0200

    GameController und Level1 erstellt

---
+++ Assets/Scripts/BlockMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMovement : MonoBehaviour
+{
+    private Transform _translation;
+
+    public float Speed;
+    // Use this for initialization
+    void Start()
+    {
+        _translation = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var wPressed = Input.GetKey(KeyCode.W);
+        var aPressed = Input.GetKey(KeyCode.A);
+        var sPressed = Input.GetKey(KeyCode.S);
+        var dPressed = Input.GetKey(KeyCode.D);
+
+        var movementVector = new Vector3(0, 0, 0);
+        if (wPressed) movementVector = Vector3.forward;
+        if (sPressed) movementVector = Vector3.back;
+        if (aPressed) movementVector = Vector3.left;
+        if (dPressed) movementVector = Vector3.right;
+
+        
+        movementVector.Normalize();
+        movementVector *= Time.deltaTime;
+        movementVector *= Speed;
+
+        _translation.Translate(movementVector);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public SceneAsset[] Scenes;
+
+    // Use this for initialization
+    void Start()
+    {
+        SceneManager.LoadScene(Scenes[0].name, LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
