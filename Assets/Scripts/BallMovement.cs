0a50f9e70da30b8c5db39dbbd39a8f9e1b066b5c
Sebastian Schwarz <sebastian.schwarz@generic.de>
2017-06-11 02:03:47 +0200

    Ball wird reflektiert

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,17 +4,31 @@ namespace Assets.Scripts
 {
     public class BallMovement : MonoBehaviour
     {
+        public float Speed = 2f;
+        private Vector3 _velocity;
+
 
         // Use this for initialization
         void Start()
         {
-
+            _velocity = Vector3.down;
         }
 
         // Update is called once per frame
         void Update()
         {
+            transform.Translate(_velocity * Time.deltaTime * Speed);
+        }
 
+        void OnCollisionEnter(Collision info)
+        {
+            foreach (var contactPoint in info.contacts)
+            {
+                //Following formula  v' = 2 * (v . n) * n - v
+                _velocity = 2 * (Vector3.Dot(_velocity, Vector3.Normalize(contactPoint.normal))) *
+                            Vector3.Normalize(contactPoint.normal) - _velocity;
+                _velocity *= -1;
+            }
         }
     }
 }
