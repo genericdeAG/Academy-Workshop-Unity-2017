763f990ee5412bbd02d83ed2a12744cb4e882903
Sebastian Schwarz <sebastian.schwarz@generic.de>
2017-06-18 13:43:58 +0200

    StartGame Manager hinzugefügt

---
+++ Assets/Scripts/StartGameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class StartGameManager : MonoBehaviour
+    {
+        public SceneAsset GameScene;
+
+        public void OnStartGame()
+        {
+            SceneManager.LoadScene(GameScene.name,LoadSceneMode.Single);
+        }
+
+        public void OnExitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
